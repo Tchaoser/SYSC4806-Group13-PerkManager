f3be73dc54900193a1c7846c31abefcfe26b4358
Peter Grose <peter.grose@outlook.com>
2025-10-10 20:21:53 -0400

    Initial commit: basic Spring Boot + Thymeleaf skeleton

---
+++ src/main/java/com/example/perkmanager/PerkmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.perkmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerkmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerkmanagerApplication.class, args);
+    }
+
+}
