781407edbb9c05c72cf95937272bddc5a44eb6c6
Kyle-Foisy <kylefoisy@telus.net>
2025-10-23 14:19:43 -0400

    Update Product.java
    Imported Table

--- src/main/java/com/example/perkmanager/Product.java
+++ src/main/java/com/example/perkmanager/Product.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /**
  * Represents a product or service for which the perk applies.
