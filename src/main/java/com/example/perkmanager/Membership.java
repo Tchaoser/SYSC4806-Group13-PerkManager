a60a2bedc9b53374ad39eee462d867ea8f4a14e4
Kyle-Foisy <kylefoisy@cmail.carleton.ca>
2025-10-22 23:40:49 -0400

    Implemented Model Version of Membership

---
+++ src/main/java/com/example/perkmanager/Membership.java
@@ -0,0 +1,63 @@
+package com.example.perkmanager;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * Represents a certain membership
+ */
+@Entity
+public class Membership {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    //What type of membership it is. Ex: Credit card, Air Miles, etc
+    private String type;
+    //The Company or Organization associated with the membership. Ex: RBC, WestJet, Cineplex, etc
+    private String organizationName;
+    //The Name/Description of the membership. Ex: West Jet Rewards Member
+    private String description;
+
+    public Membership() {
+    }
+
+    public Membership(String type, String organizationName, String description) {
+        this.type = type;
+        this.organizationName = organizationName;
+        this.description = description;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public String getOrganizationName(){
+        return organizationName;
+    }
+
+    public void setOrganizationName(String organizationName){
+        this.organizationName = organizationName;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+}
---
+++ src/test/java/com/example/perkmanager/MembershipTest.java
@@ -0,0 +1,66 @@
+package com.example.perkmanager;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MembershipTest {
+    Membership membership;
+    @BeforeEach
+    void setUp() {
+        membership = new Membership("Airline Loyalty Program", "West Jet", "West Jet Rewards Member");
+    }
+
+    @Test
+    void getId() {
+        membership.setId(2l);
+        assertEquals(2l, membership.getId());
+    }
+
+    @Test
+    void setId() {
+        membership.setId(2l);
+        assertEquals(2l, membership.getId());
+    }
+
+    @Test
+    void getType() {
+        assertEquals("Airline Loyalty Program", membership.getType());
+    }
+
+    @Test
+    void setType() {
+        membership.setType("Store Membership");
+        assertEquals("Store Membership", membership.getType());
+        assertEquals("West Jet", membership.getOrganizationName());
+        assertEquals("West Jet Rewards Member", membership.getDescription());
+
+    }
+
+    @Test
+    void getOrganizationName() {
+        assertEquals("West Jet", membership.getOrganizationName());
+    }
+
+    @Test
+    void setOrganizationName() {
+        membership.setOrganizationName("Air Canada");
+        assertEquals("Airline Loyalty Program", membership.getType());
+        assertEquals("Air Canada", membership.getOrganizationName());
+        assertEquals("West Jet Rewards Member", membership.getDescription());
+    }
+
+    @Test
+    void getDescription() {
+        assertEquals("West Jet Rewards Member", membership.getDescription());
+    }
+
+    @Test
+    void setDescription() {
+        membership.setDescription("Air Canada Rewards Member");
+        assertEquals("Airline Loyalty Program", membership.getType());
+        assertEquals("West Jet", membership.getOrganizationName());
+        assertEquals("Air Canada Rewards Member", membership.getDescription());
+    }
+}
