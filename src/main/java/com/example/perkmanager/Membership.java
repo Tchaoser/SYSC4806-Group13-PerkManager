84d549adaa4fd2d810942b63b53e4f9dffd3a6e6
Kyle-Foisy <kylefoisy@telus.net>
2025-10-23 14:19:22 -0400

    Update Membership.java
    Imported Table

--- src/main/java/com/example/perkmanager/Membership.java
+++ src/main/java/com/example/perkmanager/Membership.java
@@ -3,6 +3,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /**
  * Represents a membership used for perks.
