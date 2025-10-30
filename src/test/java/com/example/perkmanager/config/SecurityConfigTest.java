a8f51aaf901de5d098037bbe966885d1ce3a1f8c
lucaswarburton <lucas.warburton@gmail.com>
2025-10-30 00:10:42 -0400

    added tests for AccountController and SecurityConfig

---
+++ src/test/java/com/example/perkmanager/config/SecurityConfigTest.java
@@ -0,0 +1,60 @@
+package com.example.perkmanager.config;
+
+import com.example.perkmanager.security.AccountUserDetailsService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class SecurityConfigTest {
+
+    private SecurityConfig securityConfig;
+    private AccountUserDetailsService userDetailsService;
+
+    @BeforeEach
+    public void setUp() {
+        securityConfig = new SecurityConfig();
+        userDetailsService = mock(AccountUserDetailsService.class);
+
+    }
+
+    @Test
+    void passwordEncoder(){
+        Object encoder1 = securityConfig.passwordEncoder();
+        assertTrue(encoder1 instanceof BCryptPasswordEncoder);
+        String encodedPassword = ((BCryptPasswordEncoder)encoder1).encode("password");
+        BCryptPasswordEncoder encoder2 = (BCryptPasswordEncoder) securityConfig.passwordEncoder();
+        assertTrue(encoder2.matches("password", encodedPassword));
+    }
+
+    @Test
+    void authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = securityConfig.authenticationProvider(userDetailsService, securityConfig.passwordEncoder());
+
+        UserDetails user = User.withUsername("testuser")
+                .password(securityConfig.passwordEncoder().encode("pass"))
+                .roles("USER")
+                .build();
+
+        when(userDetailsService.loadUserByUsername("testuser")).thenReturn(user);
+
+        UsernamePasswordAuthenticationToken authToken =
+                new UsernamePasswordAuthenticationToken("testuser", "pass");
+
+        Authentication result = authenticationProvider.authenticate(authToken);
+
+        assertTrue(result.isAuthenticated());
+        assertEquals("testuser", result.getName());
+    }
+
+
+}
