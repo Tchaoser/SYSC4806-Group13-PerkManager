991bb1fcb506417ee5f8cc85c4dcf9349f787fd7
Kyle Foisy <kylefoisy@cmail.carleton.ca>
2025-11-08 13:52:48 -0500

    Test File for workflow v2

--- src/main/resources/templates/main.js
+++ src/main/resources/templates/main.js
@@ -2,6 +2,8 @@ function calculatesum(a, b){
     return a + b;
 }
 
+//Test 2
+
 describe("Test Suite 1", function() {
     it("test suite 1", function() {
         const result = calculatesum(5, 10)
